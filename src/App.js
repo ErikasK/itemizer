a8c85e536f464b3fb9d03b99e1bdfbeee9e296fe
ErikasK <erikasklimusinas1@gmail.com>
2022-06-09 00:10:58 +0200

    added menu selection and half of NewItemForm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Button } from "@mui/material";
+import AddNewItem from "./components/AddNewItem";
+import TrackItem from "./components/TrackItem";
+import EditITem from "./components/EditItem";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [openModal, setOpenModal] = useState(false);
+
+    const handleClose = () => setOpenModal(false);
+    const handleOpen = () => setOpenModal(true);
+
+    return (
+        <div className="App-menu">
+            <AddNewItem />
+            <TrackItem />
+            <EditITem />
+        </div>
+    );
 }
-
-export default App;
