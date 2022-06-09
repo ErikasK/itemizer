82335df6287dcdfc44bdabcd2adafd5341923dbd
ErikasK <erikasklimusinas1@gmail.com>
2022-06-09 23:04:56 +0200

    added pushing to database and retrieving from database

--- src/App.js
+++ src/App.js
@@ -3,18 +3,15 @@ import { Button } from "@mui/material";
 import AddNewItem from "./components/AddNewItem";
 import TrackItem from "./components/TrackItem";
 import EditITem from "./components/EditItem";
+import ViewAllItems from "./components/ViewAllItems";
 
 export default function App() {
-    const [openModal, setOpenModal] = useState(false);
-
-    const handleClose = () => setOpenModal(false);
-    const handleOpen = () => setOpenModal(true);
-
     return (
         <div className="App-menu">
             <AddNewItem />
             <TrackItem />
             <EditITem />
+            <ViewAllItems />
         </div>
     );
 }
